6e0401dad51818729bb9583d89c455f7d419a5ff
Tarun <tkashyap534@gmail.com>
2026-09-20 21:43:49 +0530

    C++ code to calculate peremeter of circle.

---
+++ peremeterofcircle.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+#define pi 3.14
+int main(){
+    cout<<"Peremeter of Circle.\n";
+    float r,pr;
+    cout<<"Enter raduis:";
+    cin>>r;
+    pr = 2*pi*r;
+    cout<<"Peremeter of Circle:"<<pr<<" Units"<<endl<<"Formula used:"<<"2*PI*R";
+
+    return 0;
+}
