f5b6a1b5a4826ddc7fe3f064e0546c604414e8ac
Tarun <tkashyap534@gmail.com>
2026-09-24 21:41:55 +0530

    ASKII values

---
+++ askiivalues.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char ch;
+    cout<<"Find ASKII values of digits and values:"<<endl<<"Enter word to find value:";
+    cin>>ch;
+    cout<<"Values of "<<ch<<" 5is:"<<(int)ch;
+
+    return 0;
+
+}
