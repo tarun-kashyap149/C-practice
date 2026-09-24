8a77954c79176186c41a88365e4f24c3240e7929
Tarun <tkashyap534@gmail.com>
2026-09-24 22:04:40 +0530

    Update in askii code

--- askiivalues.cpp
+++ askiivalues.cpp
@@ -4,8 +4,11 @@ int main(){
     char ch;
     cout<<"Find ASKII values of digits and values:"<<endl<<"Enter word to find value:";
     cin>>ch;
-    cout<<"Values of "<<ch<<" 5is:"<<(int)ch;
-
+    cout<<"Values of "<<ch<<" is:"<<(int)ch<<endl;
+    int x;
+    cout<<"Number to character."<<endl<<"Enter Digit: ";
+    cin>>x;
+    cout<<"Charcter is :"<<(char)x;
     return 0;
 
 }
